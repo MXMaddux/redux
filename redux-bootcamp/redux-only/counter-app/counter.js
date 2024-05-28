d5be57396cb6c408ad0a6c9ced6868521d4148a0
MXMaddux <maxmaddux@gmail.com>
2024-05-28 08:27:15 -0700

    Rearrange folders

---
+++ redux-bootcamp/redux-only/counter-app/counter.js
@@ -0,0 +1,44 @@
+// steps
+
+// initial state
+const initialState = {
+  count: 0,
+};
+
+// actions - action action creator
+// increment
+// decrement
+// reset
+// increaseByAmount
+
+// increase action creator
+const incrementAction = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+// decrease action creator
+const decrementAction = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// reset action creator
+const resetAction = () => {
+  return {
+    type: "RESET",
+  };
+};
+
+// increase by amount creator
+const increaseByAmount = () => {
+  return {
+    type: "INCREASE_BY_AMOUNT",
+  };
+};
+
+// reducer
+
+// store
